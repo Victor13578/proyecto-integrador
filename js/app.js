97594e841c20c5e199daee4fddb8432ab2eaa74b
victor <reyescardonavictormanuel333@gmail.com>
2026-09-21 11:51:05 -0500

    feat: use map to process API data

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
 const API_URL = "https://rickandmortyapi.com/api/character";
 
+const charactersContainer = document.querySelector("#characters");
+
 fetch(API_URL)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        const characters = data.results;
+
+        const cards = characters.map(character => {
+            return `
+                <article class="card">
+                    <img src="${character.image}" alt="${character.name}">
+                    <h3>${character.name}</h3>
+                    <p>${character.status}</p>
+                    <p>${character.species}</p>
+                </article>
+            `;
+        });
+
+        charactersContainer.innerHTML = cards.join("");
     })
     .catch(error => {
         console.error("Error:", error);
